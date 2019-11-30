af63ca174a593bd2a2b73c987f5b56db9cef49db
Michael Orlitzky <michael@orlitzky.com>
2019-11-29 21:47:55 -0500

    setup.py: new file to ease installation and release building.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+  name         = 'nagios-check_multiple',
+  version      = '0.0.1',
+  maintainer       = 'Clark A',
+  maintainer_email = 'clarka@gmail.com',
+  url          = 'https://github.com/clarkbox/check_multiple',
+  scripts      = ['check_multiple.py'],
+  description  = 'Run multiple Nagios checks and combine the results',
+  long_description=open('README.md').read(),
+  classifiers  = [
+    'Programming Language :: Python :: 3.6',
+    'Environment :: Console',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: MIT License',
+  ]
+)
